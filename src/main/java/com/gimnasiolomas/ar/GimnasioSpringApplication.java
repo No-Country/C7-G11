dbbcf59ef054ee4200f0c6f36399338459be0087
JoelTejerina <catrieltejerina@gmail.com>
2022-09-21 10:21:36 -0300

    Inicio del proyecto

---
+++ src/main/java/com/gimnasiolomas/ar/GimnasioSpringApplication.java
@@ -0,0 +1,13 @@
+package com.gimnasiolomas.ar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GimnasioSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GimnasioSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gimnasiolomas/ar/gimnasiospring/GimnasioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gimnasiolomas.ar.gimnasiospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimnasioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
