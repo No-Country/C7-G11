55daba20e98c3ee6c6270ae146b571317c813a46
estebanfak <estebanfak@gmail.com>
2022-10-21 14:08:31 -0300

    cambio credenciales cors en controlador de usuario y config cors

--- src/main/java/com/gimnasiolomas/ar/security/configuration/CorsConfiguration.java
+++ src/main/java/com/gimnasiolomas/ar/security/configuration/CorsConfiguration.java
@@ -15,6 +15,7 @@ public class CorsConfiguration {
                 registry.addMapping("/**")
                         .allowedOrigins("/**")
                         .allowedMethods("*")
+                        .allowedOriginPatterns("*")
                         .allowedHeaders("*");
             }
         };
